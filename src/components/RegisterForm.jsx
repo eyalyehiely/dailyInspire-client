36e55b55678826d2c050fddffa7597d28b0ac366
eyalyehiely <eyalwork0@gmail.com>
2025-04-10 19:42:34 +0300

    `Removed globe icon and associated styles from time zone select input`

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -324,9 +324,6 @@ const RegisterForm = ({ onLoginClick }) => {
                 Your Time Zone
               </label>
               <div className="relative rounded-md shadow-sm">
-                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                  <Globe className="h-5 w-5 text-indigo-500 mr-2" />
-                </div>
                 <select
                   id="timeZone"
                   name="timeZone"
